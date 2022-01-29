711d525b62a4d2e4dae8ef20d14b541d22201828
Aldo Canepa <waldoc@gmail.com>
2022-01-29 06:37:42 -0800

    Handle language for image alt and price

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { translate } from './app.internationalization';
 import { getRandomDrawings } from './db.images';
 
@@ -27,8 +27,7 @@ export class AppController {
 
   @Get('/commissions-portrait')
   @Render('commissions')
-  commissionsPortrait(@Res() res: Response) {
-    const lang = res.locals.lang.language;
+  commissionsPortrait() {
     const priceBoxes = [
       {
         image: getRandomDrawings(1, 1)[0],
@@ -36,19 +35,19 @@ export class AppController {
         sizes: [
           {
             size: { width: 18, height: 24 },
-            price: { es: 70000, en: 110 }[lang],
+            price: { es: 70000, en: 110 },
           },
           {
             size: { width: 20, height: 34 },
-            price: { es: 110000, en: 140 }[lang],
+            price: { es: 110000, en: 140 },
           },
           {
             size: { width: 30, height: 40 },
-            price: { es: 150000, en: 180 }[lang],
+            price: { es: 150000, en: 180 },
           },
           {
             size: { width: 50, height: 70 },
-            price: { es: 270000, en: 350 }[lang],
+            price: { es: 270000, en: 350 },
           },
         ],
       },
@@ -58,15 +57,15 @@ export class AppController {
         sizes: [
           {
             size: { width: 20, height: 34 },
-            price: { es: 150000, en: 180 }[lang],
+            price: { es: 150000, en: 180 },
           },
           {
             size: { width: 30, height: 40 },
-            price: { es: 200000, en: 250 }[lang],
+            price: { es: 200000, en: 250 },
           },
           {
             size: { width: 50, height: 70 },
-            price: { es: 330000, en: 300 }[lang],
+            price: { es: 330000, en: 300 },
           },
         ],
       },
--- src/db.images.ts
+++ src/db.images.ts
@@ -1,36 +1,136 @@
 import { sampleSize } from 'lodash';
 
 export const imageFilenames = [
-  { name: 'Atom.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'Baco.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'Bowie.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'Briso.jpg', subjects: 1, alt: { en: '', es: '' } },
+  {
+    name: 'Atom.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'Baco.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'Bowie.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'Briso.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
   {
     name: 'caballo.jpg',
     subjects: 1,
     alt: { en: 'Horse drawing', es: 'Dibujo caballo' },
   },
-  { name: 'Cano.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'Coca.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'IMG_20210820_090530.jpg', subjects: 2, alt: { en: '', es: '' } },
-  { name: 'img019.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'img025.jpg', subjects: 2, alt: { en: '', es: '' } },
-  { name: 'img035.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'img038.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'img039.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'img041.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'img048.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'img054.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'img091.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'img094.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'img095.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'jack.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'Komatsu.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'lukas2.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'Malu.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'Negrita.jpg', subjects: 1, alt: { en: '', es: '' } },
-  { name: 'sammy y benito tiny.jpg', subjects: 2, alt: { en: '', es: '' } },
-  { name: 'Valentin.jpg', subjects: 1, alt: { en: '', es: '' } },
+  {
+    name: 'Cano.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'Coca.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'IMG_20210820_090530.jpg',
+    subjects: 2,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'img019.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'img025.jpg',
+    subjects: 2,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'img035.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'img038.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'img039.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'img041.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'img048.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'img054.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'img091.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'img094.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'img095.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'jack.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'Komatsu.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'lukas2.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'Malu.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'Negrita.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'sammy y benito tiny.jpg',
+    subjects: 2,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
+  {
+    name: 'Valentin.jpg',
+    subjects: 1,
+    alt: { en: 'sample english alt text', es: 'ejemplo alt espanol' },
+  },
 ];
 
 export const getRandomDrawings = (count = 4, subjects = undefined) => {
